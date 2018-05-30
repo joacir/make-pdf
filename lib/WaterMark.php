4385cb6fb2ee55fd907a98450d2db564e82c9dd5
Renata Sayuri Matsubara <renata.sayuri@aelian.com.br>
2018-05-30 14:57:23 -0300

    Correcao cor da marca d'agua
    Correcao cor da marca d'agua

--- lib/WaterMark.php
+++ lib/WaterMark.php
@@ -6,11 +6,10 @@ class WaterMark extends Cell {
     public function create() {
         $x = $this->GetX();
         $y = $this->GetY();
+        $this->config['textColor'] = '205,205,205' ;
         $angle = $this->getAngle();
         $this->Pdf->Rotate($angle, $x, $y);
-        $this->Pdf->SetTextColor(205, 205, 205);
         parent::create();
-        $this->Pdf->SetTextColor(0, 0, 0);
         $this->Pdf->Rotate(0);
     }
         
