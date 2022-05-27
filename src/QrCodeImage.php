bcd5d197cf5866f464c4d9fafda47dc6e3963ef4
joacir <jometal@gmail.com>
2022-05-27 11:55:00 -0300

    Inclusão do novo parâmetro de version no QrCodeImage

--- src/QrCodeImage.php
+++ src/QrCodeImage.php
@@ -6,20 +6,20 @@ use chillerlan\QRCode\QROptions;
 use Pdf\MakePdf\Cell;
 
 class QrCodeImage extends Cell {
-    
+
     public function create() {
-        $x = $this->GetX();        
+        $x = $this->GetX();
         $y = $this->GetY();
         $w = $this->getLineWidth();
         $h = $this->getLineHeight();
 
-        $qrCode = $this->getQrCode();        
+        $qrCode = $this->getQrCode();
 
         $this->Pdf->Image($qrCode, $x, $y, $w, $h, 'PNG');
-        
+
         $this->Pdf->SetY($y + $h);
         $this->Pdf->SetX($x + $w);
-        $this->Pdf->setLasth($h + $this->titleLineHeight);        
+        $this->Pdf->setLasth($h + $this->titleLineHeight);
     }
 
     public function getQrCode() {
@@ -35,9 +35,18 @@ class QrCodeImage extends Cell {
 
     public function getQrOptions() {
         return new QROptions([
-            'version' => 5,
+            'version' => $this->getVersion(),
             'outputType' => QRCode::OUTPUT_IMAGE_PNG,
             'eccLevel' => QRCode::ECC_L,
-        ]);        
+        ]);
+    }
+
+    public function getVersion() {
+        $version = 5;
+        if (isset($this->config['version'])) {
+            $version = $this->config['version'];
+        }
+
+        return $version;
     }
 }
