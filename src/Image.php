b75f74e1fcf1e7da8ad35cc33887ffc3459e3f49
Bárbara Kellen <barbara.kcbs@gmail.com>
2024-05-24 15:33:38 -0300

    Verificando orientação da imagem para rotacionar pra posição correta.

--- src/Image.php
+++ src/Image.php
@@ -4,20 +4,27 @@ namespace Pdf\MakePdf;
 use Pdf\MakePdf\Cell;
 
 class Image extends Cell {
-    
+
     public function create() {
-        $x = $this->GetX();        
+        $x = $this->GetX();
         $y = $this->GetY();
         $w = $this->getLineWidth();
         $h = $this->getLineHeight();
-        $imageFile = $this->getImageFile();        
-
+        $imageFile = $this->getImageFile();
+        $exif = exif_read_data($imageFile);
+        if (!empty($exif['Orientation'])) {
+            $orientation = $exif['Orientation'];
+            $rotations = [3 => 180, 6 => -90, 8 => 90];
+            $angle = $rotations[$orientation];
+            $this->Pdf->Rotate($angle, $x + $w / 2, $y + $h / 2);
+        }
+        
         $this->Pdf->Image($imageFile, $x, $y, $w, $h);
         $this->Pdf->SetY($y + $h);
         $this->Pdf->SetX($x + $w);
-        $this->Pdf->setLasth($h + $this->titleLineHeight);        
+        $this->Pdf->setLasth($h + $this->titleLineHeight);
     }
-    
+
     public function getImageFile() {
         return $this->getText();
     }
